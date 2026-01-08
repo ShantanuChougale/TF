6b0919615f03c05ef1ae5d177cc7cc8e990bf86a
Shantanu Chougale <shantanuchougale@gmail.com>
2026-01-08 16:00:27 +0530

    day-4-state

---
+++ Day-4-state/backend.tf
@@ -0,0 +1,10 @@
+ terraform {
+   backend "s3" {
+    bucket = "shantanudev"
+    key ="day-4/terraform.tfstate"
+    region = "us-east-1"
+     #Enable s3 native locking
+     use_lockfile = true
+     # The dynamodb_table argument is no longer needed
+   }
+ }
