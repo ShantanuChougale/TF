5d3e56e6df877fc304b552a4424bc9b0453c836b
Shantanu Chougale <shantanuchougale@gmail.com>
2026-01-15 14:52:01 +0530

    day-6 file2

---
+++ Day-6-depends-on/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "name" {
+  ami = "ami-068c0051b15cdb816"
+  instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "name" {
+  bucket = "aesfdgrsdfsr"
+  depends_on = [ aws_instance.name ]
+}
